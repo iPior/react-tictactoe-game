c112dc5adaae3bd5675ccbd4a7d826b5ed9e1a79
Piotr Szaran <szaranpiotr@gmail.com>
2025-01-06 23:40:00 -0500

    figured out how to send api to and from the backend

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
+const claude = require('./utils/claude.js')
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8080;
-
-const cors = require('cors');
 const corsOptions = {
   origin: 'http://localhost:5173'
 }
 
 app.use(cors(corsOptions));
+app.use(express.json());
 
-app.get('/api/claude', (req, res) => {
-  res.json({message: 'Hello'});
+app.post('/claude', (req, res) => {
+  const { gameState, availableMoves } = req.body;
+
+  claude.getMoveFromClaude(gameState, availableMoves)
+    .then(response => {
+      res.json(response.index); // send back response
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error while communicating with Claude. Error: ${err}` }); // send back error
+    });
 });
 
 app.listen(PORT, () => 
