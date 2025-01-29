1fea8e950e3b4e77a21c84de0e4149a231755c91
Piotr Szaran <szaranpiotr@gmail.com>
2025-01-28 21:10:23 -0500

    adding a prerender to speed up claudes response

--- server/utils/claude.js
+++ server/utils/claude.js
@@ -35,8 +35,14 @@ export async function getMoveFromClaude(gameBoard, availableMoves) {
     try{ 
       const msg = await anthropic.messages.create({
         model: "claude-3-5-sonnet-20241022", // claude-3-haiku-20240307
-        max_tokens: 2000,
-        system: ANTHROPIC_PROMPT,
+        max_tokens: 2048,
+        system: [
+        {
+          type: "text",
+          text: ANTHROPIC_PROMPT,
+          cache_control: {type: "ephemeral"}
+        },
+      ],
         messages: [
           { 
             role: "user", 
@@ -47,9 +53,35 @@ export async function getMoveFromClaude(gameBoard, availableMoves) {
           }
         ],
       });
+      console.log(msg)
       return JSON.parse(msg.content[0].text)
 
     } catch(error) {
       console.error(error)
     }
   }
+
+export async function claudePreRender() {
+  try{ 
+    const msg = await anthropic.messages.create({
+      model: "claude-3-5-sonnet-20241022", // claude-3-haiku-20240307
+      max_tokens: 2048,
+      system: [
+        {
+          type: "text",
+          text: ANTHROPIC_PROMPT,
+          cache_control: {type: "ephemeral"}
+        },
+      ],
+      messages: [
+          { 
+            role: "user", 
+            content: "Preloading prompt. Please remember to return only a valid JSON object containing a single 'index' field, which is a number between 0-8 representing the index of the position you want to play",
+          }
+        ],
+    });
+    console.log(msg)
+  } catch(error) {
+    console.error(error)
+  }
+}
